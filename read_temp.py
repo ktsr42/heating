f2b574f250fdf51e96f625719e6e0e60eb3d7819
Klaas Teschauer <1313262+ktsr42@users.noreply.github.com>
2018-02-23 18:41:38 -0500

    Added the creation of the publishing user to the CF template and fixed the publishing script.

--- read_temp.py
+++ read_temp.py
@@ -11,11 +11,12 @@ import botocore.config as awsconfig
 import boto3
 
 
-TemperatureSensorFile="/sys/bus/w1/devices/<serial>/w1_slave"
-History="temperature_history"
-ReadingFilePrefix="pipe_temperature."
-MaxReadings = 100
-AwsParameters = ('region', 'key_id', 'secret_key', 'bucket', 'path', 'prefix')
+#TemperatureSensorFile="/sys/bus/w1/devices/<serial>/w1_slave"
+#History="temperature_history"
+#ReadingFilePrefix="pipe_temperature."
+#MaxReadings = 100
+
+AwsParameters = ('region', 'key_file', 'bucket', 'path', 'prefix')
 
 def read_current_temperature(srcfile):
     with open(srcfile) as tf:
@@ -24,14 +25,14 @@ def read_current_temperature(srcfile):
     (_, tag, temperature_reading) = temperature_reading_line.partition('t=')
     if tag != 't=':
         raise Exception('Could not parse temperature sensor file')
-    return (time.time(), int(temperature_reading) / 1000)
+    return (int(time.time()), int(temperature_reading) / 1000)
 
 def get_last_reading_number(targetdir, prefix):
-    old_readings = [int(f[len(prefix):]) for f in os.listdir(targetdir) if f[:len(prefix)] == prefix]
-    if len(old_readings) == 0:
-        return 0
-    else:
-        return max(old_readings)
+    try:
+        old_readings = sorted((f for f in pathlib.Path(targetdir).iterdir() if f.name[:len(prefix)] == prefix), key=lambda f:f.stat().st_mtime, reverse=True)        
+        return int(old_readings[0].name[len(prefix):])
+    except IndexError:
+        return -1
 
 def order(n0):
     if n0 == 0:
@@ -43,17 +44,17 @@ def order(n0):
         n = n // 10
     return o
 
-def write_reading(targetdir, prefix, reading):
-    num = (get_last_reading_number(targetdir, prefix) + 1) % MaxReadings
-    fname = "{0}/{1}{3:0{2}}".format(History, ReadingFilePrefix, order(MaxReadings), num)
+def write_reading(targetdir, prefix, maxreadings, reading):
+    num = (get_last_reading_number(targetdir, prefix) + 1) % maxreadings
+    fname = "{0}/{1}{3:0{2}}".format(targetdir, prefix, 1 + order(maxreadings), num)
     with open(fname, "w") as f:
-        print("{0} {1}".format(time.time(), reading), file=f)
+        print("{0} {1}".format(*reading), file=f)
     
 def create_aws_message(srcdir, prefix):
     sdir = pathlib.Path(srcdir)
     allreadings_t = [e.read_text().strip().split() for e in sdir.iterdir() if e.is_file() and e.name[:len(prefix)] == prefix]
-    kf = lambda x: x[0]
-    return json.dumps(sorted(((int(ts), r) for ts, r in allreadings_t), key = kf))
+    kf = lambda x: x['timestamp']
+    return json.dumps(sorted((dict(timestamp=int(ts), temperature=float(r)) for ts, r in allreadings_t), key = kf), indent=2)
 
 def aws_upload(params, data, suffix=".json"):
     fname = "{}/{}{:%Y%m%dT%H%m%S}{}".format(params.path, params.prefix, datetime.datetime.utcnow(), suffix)
@@ -63,20 +64,19 @@ def aws_upload(params, data, suffix=".json"):
     s3obj.put(Body=data.encode())
 
 def read_config(fname):
-    config = configparser.Parser()
+    config = configparser.ConfigParser()
     config.read(fname)
     args = argparse.Namespace()
     sect_input = config['Input']
     args.sensorfile = sect_input['Sensorfile']
     args.local_history = sect_input['HistoryDir']
     args.local_file_prefix = sect_input['FilePrefix']
-    args.max_readings = sect_input['MaxReadings']
-    aws_params = argparse.Namespace()
-    for k in AwsParameters:
-        aws_params.__setattr__[k] = config['AWS'][k]
+    args.max_readings = int(sect_input['MaxReadings'])
+    aws_params = argparse.Namespace(key_id = None, secret_key = None, **{k:config['AWS'][k] for k in AwsParameters})
+    keyfile = json.load(open(aws_params.key_file))
+    aws_params.key_id = keyfile['AccessKey']['AccessKeyId']
+    aws_params.secret_key = keyfile['AccessKey']['SecretAccessKey']
     args.aws_params = aws_params
-    secret_key = pathlib.Path(aws_params.secret_key).read_bytes()
-    aws_params.secret_key = secret_key
     return args
     
 def main():
@@ -85,11 +85,11 @@ def main():
     
     # read temperature
     print("Reading the current temperature...")
-    reading = read_current_temperator(params.sensorfile)
+    reading = read_current_temperature(params.sensorfile)
     print("Current temperature is {}".format(reading))
     
     # write locally
-    write_reading(args.local_history, args.local_file_prefix, reading)
+    write_reading(args.local_history, args.local_file_prefix, args.max_readings, reading)
     
     # create aws s3 object contents
     print("Create AWS file")
