a953e7a7941a0eba13653d47a56f37c9757f4e64
Klaas Teschauer <1313262+ktsr42@users.noreply.github.com>
2018-02-27 17:57:03 -0500

    Added the scheduled event setup to the CF template and some handler code.

--- lambda_function.py
+++ lambda_function.py
@@ -7,21 +7,27 @@ import boto3
 print('Loading function')
 
 s3 = boto3.client('s3')
-sns = boto3.client('sns')
+# sns = boto3.client('sns')
 
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
-
-    # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+    print('context: {}'.format(context))
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        sns.publish(PhoneNumber='+15515802081', Message='AWS Lambda saw a new upload: ' + key)
-        return response['ContentType']
+        if 'Records' in event:
+            for rec in event['Records']:
+                if rec.get('eventSource','') == 'aws:s3':
+                    print('Received s3 event')
+                    bucket = rec['s3']['bucket']['name']
+                    key = urllib.parse.unquote_plus(rec['s3']['object']['key'])
+                    obj = s3.get_object(Bucket = bucket, Key = key)
+                    print('Object s3://{}/{}, type {}'.format(bucket,key, obj['ContentType']))
+                    # sns.publish(PhoneNumber='+15515802081', Message='AWS Lambda saw a new upload: ' + key)
+        elif event.get('source', '') == 'aws.events':
+            print('Received scheduled event, time {}, resource {}'.format(event['time'], event['resources'][0]))
+        else:
+            print("Received unexpected event, ignoring")
+        return
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
--- update_lambda_package.sh
+++ update_lambda_package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 zip nx.zip lambda_function.py
-aws --profile root s3 cp nx.zip s3://ktsr42.s3.code/code/
-aws --profile root lambda update-function-code --s3-bucket ktsr42.s3.code --s3-key code/nx.zip --function-name ktsr42_lambda_test2
+# aws --profile root s3 cp nx.zip s3://ktsr42.s3.code/code/
+# aws --profile root lambda update-function-code --s3-bucket ktsr42.s3.code --s3-key code/nx.zip --function-name ktsr42_lambda_test2
+aws --profile root lambda update-function-code --zip-file fileb://nx.zip --function-name ktsr42_lambda_test2
