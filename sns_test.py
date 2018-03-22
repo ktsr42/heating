a50230eaf6c8d1a080e807411c8266b80678ba6c
Klaas Teschauer <1313262+ktsr42@users.noreply.github.com>
2018-03-22 17:21:22 -0400

    Reorg/cleanup of the source tree.

--- sns_test.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-def main():
-    session = boto3.Session(profile_name = 'root')
-    sns = session.client('sns')
-    sns.publish(PhoneNumber='+15515802081', Message='Hello again from folio')
-
-if __name__ == '__main__':
-    main()
