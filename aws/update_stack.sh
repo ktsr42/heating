1c423a4ddcb4d5096618b15b154fba011d8bfa32
Klaas Teschauer <1313262+ktsr42@users.noreply.github.com>
2018-04-12 18:16:44 -0400

    Automated the AWS setup to the best of current abilities.

--- aws/update_stack.sh
+++ aws/update_stack.sh
@@ -9,7 +9,7 @@ CODE_BUCKET_PATH=code
 
 PROFILE=root
 
-Stackname="${PREFIX}_monitor_heating"
+Stackname="${PREFIX}MonitorHeating"
 
 Sensorfile=/home/klaas/Projects/heating/testdata/sensor
 ReaderUser=tsensor
@@ -19,22 +19,34 @@ AWSRegion=us-east-1
 make
 
 # Put the zipfile into S3
-aws --profile $PROFILE s3 sync $ZIPFILE s3://$CODE_BUCKET/$CODE_BUCKET_PATH/$ZIPFILE
+aws --profile $PROFILE s3 cp $ZIPFILE s3://$CODE_BUCKET/$CODE_BUCKET_PATH/$ZIPFILE
 
-# Create/update the stack
-aws --profile $PROFILE cloudformation wait deploy \
-    --stack-name $Stackname \
-    --template-file aws_setup.yaml \
-    --parameter-overrides CommonPrefix=$PREFIX
-    --capabilities CAPABILITY_IAM
+if aws --profile $PROFILE cloudformation list-stacks \
+	| jq '.StackSummaries|.[] | select(.StackStatus != "DELETE_COMPLETE") | .StackName' \
+	| grep -q $Stackname
+then
+    ## Update stack
+    aws --profile $PROFILE cloudformation update-stack \
+	--stack-name $Stackname \
+	--template-body file://aws_setup.yaml \
+	--parameters ParameterKey=CommonPrefix,ParameterValue=$PREFIX ParameterKey=BucketName,ParameterValue=$PREFIX.heating.s3 \
+	--capabilities CAPABILITY_NAMED_IAM
 
-# Create the access key if this was the stack creation
-set +e
-aws --profile $PROFILE iam create-access-key ${PREFIX}_heating_publisher > ../keys/${PREFIX}_heating_publisher_key.json
-set -e
+    aws --profile $PROFILE cloudformation wait stack-update-complete --stack-name $Stackname
+else
+    ## create stack
+    aws --profile $PROFILE cloudformation create-stack \
+	--stack-name $Stackname \
+	--template-body file://aws_setup.yaml \
+	--parameters ParameterKey=CommonPrefix,ParameterValue=$PREFIX ParameterKey=BucketName,ParameterValue=$PREFIX.heating.s3 \
+	--capabilities CAPABILITY_NAMED_IAM
 
-cat > new-read_temp_config.ini  <<EOF
-[Input]
+    aws --profile $PROFILE cloudformation wait stack-create-complete --stack-name $Stackname
+    
+    aws --profile $PROFILE iam create-access-key --user-name ${PREFIX}_heating_publisher > ../keys/${PREFIX}_heating_publisher_key.json
+    
+    cat > new-read_temp_config.ini  <<EOF
+[Input]	
 Sensorfile=$Sensorfile
 HistoryDir=/home/$ReaderUser/data
 Logfiles=/home/$ReaderUser/logs/reader_log
@@ -44,10 +56,11 @@ LogLevel=info
 
 [AWS]
 region=$AWSRegion
-key_file=/home/$ReaderUserreleases/prod/access_key.json
+key_file=/home/$ReaderUser/releases/prod/access_key.json
 bucket=$PREFIX.heating.s3
 path=observations
 prefix=obs
 EOF
 
-mv -vf new-read_temp_config.ini ../reader
+    mv -vf new-read_temp_config.ini ../reader
+fi
